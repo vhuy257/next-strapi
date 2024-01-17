73a91f1f4fdadb747c55c3883c2ee20fe7b2f9a0
vhuy2571990@gmail.com <ldcc.vinhhuy>
2024-01-17 11:17:39 +0700

    Strapi security block image

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,18 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  // {
+  //   name: 'strapi::security',
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         'connect-src': ["'self'", 'https:'],
+  //         'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
+  //         'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
 ];
