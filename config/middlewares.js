af6452c840ee8fe913c7b73ddcfec18250311464
vhuy2571990@gmail.com <ldcc.vinhhuy>
2024-01-17 11:26:06 +0700

    Security block image

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,7 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,18 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  // {
-  //   name: 'strapi::security',
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
-  //         'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'trusty-desk-8c3c7017fe.strapiapp.com'],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'trusty-desk-8c3c7017fe.strapiapp.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'trusty-desk-8c3c7017fe.strapiapp.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
